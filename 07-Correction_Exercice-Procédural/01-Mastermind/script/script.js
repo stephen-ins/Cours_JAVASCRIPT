2f47ba4ac22f921a4311faea3786bd82e8f275e7
stephen.insixiengmay <stephen.insixiengmay@gmail.com>
2024-12-06 08:04:01 +0100

    update debex vendredi 06/12 - Distanciel

--- 07-Correction_Exercice-Procédural/01-Mastermind/script/script.js
+++ 07-Correction_Exercice-Procédural/01-Mastermind/script/script.js
@@ -3,27 +3,26 @@ const colorsLenght = colors.length;
 const circleSolution = document.querySelectorAll(".circle__solution");
 const circleChoices = document.querySelectorAll(".circle__choices");
 const choicesCurrent = document.querySelector(".choices").innerHTML;
+const modal = document.querySelector(".modal");
+const content = document.querySelector(".container");
+const itemTentative = document.querySelector(".numberTentatives");
 let compteur = 0;
 let step = 1;
 let handleSelectedColors = [];
 let arrayMatchs = [];
 let arrayColorsIndicators = [];
-
-// console.log(circleChoices);
-// console.log(circleSolution);
-// console.log(colors);
-// console.log(colorsLenght);
-
-// array = toutes les couleurs selectionné par l'utilisateur
-// str = 1 couleur de la combinaison secrete
+let nbTentatives = 12;
+// arr = toute les couleurs selectionnées par l'utilisateur
+// str = 1 couleur combinaison secrète
+// handleSelectedColors, blue
 const getMatch = (arr, str) => {
-  console.log(arr);
-  console.log(str);
-  //                    bleu
+  //   console.log(arr);
+  //   console.log(str);
+  //                bleu
   let reg = new RegExp(str, "i");
-  //                   1 couleur
+  //               1 couleur, data-position
   return arr.filter((item, index) => {
-    // Si la couleur selectionné par l'internaute correspond au couleur de la combinaison cachée
+    // si la couleur selectionné par l'internaute correspond à une couleur de la combinaison secrète
     if (item.match(reg)) {
       // arrayMatchs[2] = blue
       // arrayMatchs[data-position] = blue
@@ -35,12 +34,17 @@ const getMatch = (arr, str) => {
 
 const randomSelection = (n) => {
   let newArr = [];
+  if (n >= colorsLenght) {
+    return colors;
+  }
+  //              0 <= 3
   for (let i = 0; i <= n; i++) {
+    //            colors[   0.45     *         6    ] = indice entre 0 et 6 maximum
+    //            colors[0] = indice entre 0 et 6 maximum
+    //            colors[3] = indice entre 0 et 6 maximum
     let newElem = colors[Math.floor(Math.random() * colorsLenght)];
-
-    // console.log("couleur" + i);
-    // console.log("colors:" + newElem);
-
+    // console.log('couleur ' + i);
+    // console.log('couleur ' + newElem);
     while (newArr.includes(newElem)) {
       newElem = colors[Math.floor(Math.random() * colorsLenght)];
     }
@@ -50,11 +54,9 @@ const randomSelection = (n) => {
 };
 
 const arrayRandomColors = randomSelection(3);
-// console.log("la combinaison cachée :", arrayRandomColors);
 
 circleSolution.forEach((el, index) => {
-  //   console.log(el);
-  //   console.log(index);
+  //               arrayRandomColors[0] = 'red';
   el.classList.add(arrayRandomColors[index]);
 });
 
@@ -64,14 +66,13 @@ const allowDrop = (event) => {
 };
 
 const dragstart = (event) => {
-  // console.log(event.target.id);
-  let id = event.target.id + "step" + step;
-  // console.log(id);
+  let id = event.target.id + "-step" + step; // green-step2
+
   event.target.setAttribute("id", id);
   event.dataTransfer.setData("text/plain", id);
 };
 
-// Cette fonction permet de réinitialiser les choix de couleurs aprés chaque étapes
+// Cette fonction permet de réinitialiser les choix de couleurs après chaque étapes
 const reset = () => {
   const choices = document.querySelector(".choices");
   choices.innerHTML = choicesCurrent;
@@ -81,70 +82,109 @@ const drop = (event) => {
   event.preventDefault();
 
   compteur++;
-  // console.log(compteur);
 
   let data = event.dataTransfer.getData("text/plain");
-  // console.log(data);
   event.target.append(document.getElementById(data));
 
-  // Une fois le drag and drop effectué, on ne peut plus déplacer l element au bout de 500ms, setTimeout() s execute automatiquement au bout de 500ms.
+  // Une fois le drag and drop effectué, on ne peut plus déplacer l'éléments au nout de 500ms, setTimeout() s'execute automatiquement au bout de 500ms
   setTimeout(() => {
     document.getElementById(data).setAttribute("draggable", false);
   }, "500");
 
   const position = event.currentTarget.getAttribute("data-position");
-  // console.log(position);
   const color = document.getElementById(data).getAttribute("data-color");
-  // console.log(color);
+  event.currentTarget.classList.add("user__select");
 
+  // handleSelectedColors[2] = blue
   handleSelectedColors[position] = color;
-  console.log(handleSelectedColors);
+  //   console.log(handleSelectedColors);
 
   // On entre dans cette condition tout les 4 drop
   if (compteur % 4 == 0) {
     // On boucle les couleurs de la combinaison secrète
     arrayRandomColors.forEach((colorRandom, indexRandom) => {
-      // console.log(colorRandom);
-      // console.log(indexRandom);
       getMatch(handleSelectedColors, colorRandom);
-      // console.log(arrayMatchs);
+      //   console.log(arrayMatchs);
+
+      // On convertit les tableaux de couleurs en chaine de caractères, si les deux chaines sont bien égales, l'utilisateur a trouvé la combinaison
       if (arrayRandomColors.toString() === arrayMatchs.toString()) {
-        // console.log("Vous avez gagné");
+        console.log("vous avez gagné");
+
         arrayColorsIndicators.push("green");
+
+        setTimeout(() => {
+          modal.classList.add("modalIn");
+          modal.children[0].innerText =
+            "Félicitations !! Vous avez trouvé la combinaison";
+          content.classList.add("content__modal");
+        }, "2000");
       } else {
-        // On entre dans le else si il y a des correspondances de couleurs mais pas au bon endroit
-        // console.log("On y est presque");
+        // On entre dans le else si il y a des correspondances de couleurs
+        console.log("pas gagné");
+
         arrayMatchs.forEach((colorMatch, indexMatch) => {
           if (colorRandom == colorMatch && indexRandom == indexMatch) {
-            console.log("colorMatch " + colorMatch + " bien positionné");
+            // bonne couleur bien positionné
+            console.log("ColorMatch " + colorMatch + " bien positionné");
             arrayColorsIndicators.push("green");
           }
           if (colorRandom == colorMatch && indexRandom != indexMatch) {
+            // bonne couleur mal positionné
             console.log(
-              "colorMatch " + colorMatch + " existante mais mal positionné"
+              "ColorMatch " + colorMatch + " existante mais mal positionnée"
             );
             arrayColorsIndicators.push("orange");
           }
         });
       }
     });
+
+    // permet de lister les éléments de l'array par ordre alphabétique
+    arrayColorsIndicators.sort();
     console.log(arrayColorsIndicators);
+    const dataIndice = document.querySelectorAll(
+      '[data-indice="' + step + '"]'
+    );
+    console.log(dataIndice);
+    arrayColorsIndicators.forEach((couleur, index) => {
+      dataIndice[index].classList.add(couleur);
+    });
 
-    // On compare la combinaison cachée avec la combinaison choisie (en chaine de caractères, toString
-    // Si bien égales, le joueur a trouvé la combinaison cachée
-
+    nbTentatives--;
     step++;
-    // console.log("prochaine étape :" + step);
-
     reset();
+
+    itemTentative.innerHTML = nbTentatives;
+
+    if (step > 12) {
+      modal.classList.add("modalIn");
+      modal.children[0].innerText = "Vous avez perdu !!";
+      content.classList.add("content__modal");
+    }
 
     const positionStep = document.querySelectorAll(
       '[data-step="' + step + '"]'
     );
-    // console.log(positionStep);
     positionStep.forEach((element) => {
       element.setAttribute("ondrop", "drop(event)");
       element.setAttribute("ondragover", "allowDrop(event)");
     });
+
+    // On réinitialise
+    arrayMatchs = [];
+    arrayColorsIndicators = [];
   }
+};
+
+const descoverResult = () => {
+  circleSolution.forEach((el) => {
+    el.classList.remove("hide");
+
+    setTimeout(() => {
+      modal.classList.add("modalIn");
+      modal.children[0].innerText =
+        "Perdu !! Voici la combinaison : " + arrayRandomColors;
+      content.classList.add("content__modal");
+    }, "1000");
+  });
 };
---
+++ 07-Correction_Exercice-Procédural/02-Les_Artistes_Peintres/scripts/components/Header.js
@@ -0,0 +1,59 @@
+export const Header = (datas) => {
+  //   console.log(datas);
+  let navLink = "";
+  // Picasso in object toute les données de Picasso
+  for (let key in datas) {
+    // console.log(key);
+    navLink += ` <li class="nav__item">
+              <a href="?peintre=${key}" class="nav__link">${key}</a>
+             </li>`;
+  }
+
+  //   console.log(navLink);
+
+  return ` <header class="header">
+        <div class="header__logo">
+          <a href="index.html" class="header__link__logo">
+            <img
+              src="assets/logos/logo-art-peinture.png"
+              alt="logo"
+              class="logo"
+            />
+          </a>
+          <h1 class="header__title">Les Artistes Peintres</h1>
+        </div>
+        <i class="fa-solid fa-bars icone__burger"></i>
+        <nav class="nav">
+          <ul class="nav__list">${navLink}</ul>
+        </nav>
+      </header>
+    `;
+};
+
+// Exo : créer une fonction permettant de gérer le menu déroulant en cliquant sur le bouton hamburger
+
+// iconeBurger.addEventListener("click", showMenuBurger);
+// console.log("click: ", iconeBurger);
+
+export const showMenuBurger = () => {
+  const iconeBurger = document.querySelector(".icone__burger");
+  const navList = document.querySelector(".nav__list");
+  console.log(navList);
+  iconeBurger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  });
+};
+
+// Exo : créer une fonction pour les liens actifs, si dans l'url ?peintre=Picasso, le lien <a>"Picasso" devient actif
+
+export const activeNavLink = () => {
+  const navLinkActive = document.querySelectorAll(".nav__link");
+  navLinkActive.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      navLinkActive.forEach((link) =>
+        link.classList.remove("nav__link__active")
+      );
+      this.classList.add("nav__link__active");
+    });
+  });
+};
